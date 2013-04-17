c168fdf458a2685510d42eb520f59a16f3e1a157
marcelo <intohybrid@gmail.com>
2013-04-16 23:07:55 -0400

    cambios

---
+++ Coffiers/gen/cl/udla/aci810/coffiers/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cl.udla.aci810.coffiers;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Coffiers/src/cl/udla/aci810/coffiers/SplashActivity.java
@@ -0,0 +1,35 @@
+package cl.udla.aci810.coffiers;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		lanzarThread();
+	}
+
+	private void lanzarThread(){
+		Thread timer = new Thread(){
+			public void run(){
+				try {
+					sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally{
+					Intent intent = new Intent(SplashActivity.this, PrincipalActivity.class);
+					startActivity(intent);
+				}
+			}
+		};
+		timer.start();
+		
+		
+	}
+
+}
